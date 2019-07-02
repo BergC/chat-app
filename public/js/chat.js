716e0ff6c16a3894e4d4d6b5c62afad7a47e1c50
Chris <christophertberg@gmail.com>
2019-07-01 17:44:19 -0700

    Rendering messages to the browser.

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,10 +5,20 @@ const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
 const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 // Send a message.
 socket.on('message', (message) => {
     console.log(message);
+
+    const html = Mustache.render(messageTemplate, {
+        message
+    });
+    
+    $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm.addEventListener('submit', (e) => {
